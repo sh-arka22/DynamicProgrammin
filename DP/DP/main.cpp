69ba94a3ead9fe8f34428981f0a71e1c93fee334
arko <arkajyotisaha891@gmail.com>
2022-08-17 20:20:58 +0530

    template creation

---
+++ DP/DP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DP
+//
+//  Created by Arkajyoti Saha on 17/08/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "ok\n";
+    return 0;
+}
--- template.cpp
+++ template.cpp
@@ -9,8 +9,23 @@ void file_i_o(){
 	#endif
 }
 //######################################################################################################################################################################
+int n,m;
+void display1D(int *dp){
+    for(int i=0;i<n;i++){
+        cout<<dp[i]<<" ";
+    }
+}
+void display2D(int **dp){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 
 int main(){
     file_i_o();
-    
+    cin>>n;
 }
