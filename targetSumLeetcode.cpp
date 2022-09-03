dd0ffad2384f02626cbe13a70932ab55f64516e4
arko <arkajyotisaha891@gmail.com>
2022-09-03 22:45:39 +0530

    kaam samjh aya watch STRIVER LEC 26

--- targetSumLeetcode.cpp
+++ targetSumLeetcode.cpp
@@ -37,9 +37,30 @@ int recc(vector<int> &nums, int n, int tar, int sum){
     return dp[n][tar] = cnt;
 }
 
-//METHOD 2  watch striver lec 26
+//shifting from 0 to sum 
+int recc(vector<int> &nums, int n, int tar, int sum){
+    if(n==0) return dp[n][sum] = (tar==sum);
+    if(dp[n][sum] != -1) return dp[n][sum];
+    int cnt = 0;
+
+    cnt += recc(nums,n-1, tar , sum + (nums[n-1]));
+    cnt += recc(nums,n-1, tar , sum - (nums[n-1]));
+
+    return dp[n][sum] = cnt;
+}
+
+int findTargetSumWays(vector<int>& nums, int target) {
+    int sum = accumulate(begin(nums), end(nums), 0);
+    if(sum < target or sum < -target) return 0;
+    dp.resize(nums.size()+1, vector<int>(2*sum+1, -1));
+    return recc(nums,nums.size(), sum + target, sum);
+}
+
+
+
 
 //************************************************************************************************
+//METHOD 2  watch striver lec 26
 int findTargetSumWays(vector<int>& nums, int target) {
     int sum = accumulate(begin(nums), end(nums), 0);
     if(sum < target or sum < -target) return 0;
