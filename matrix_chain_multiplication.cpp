75fabce697bd8d6baddcf0dc8e7adb815f561a3d
arko <arkajyotisaha891@gmail.com>
2022-09-07 22:14:28 +0530

    lecture not not completed

--- matrix_chain_multiplication.cpp
+++ matrix_chain_multiplication.cpp
@@ -24,7 +24,24 @@ void display2D(vector<vector<int>>dp){
     }
 }
 
+const static int N = 1e4;
+int dp[N][N];
+
+int mcm(int arr[], int si, int ei){
+    if(ei = si+1) return dp[si][ei] = 0;
+    if(dp[si][ei] != 0 ) return dp[si][ei];
+    int mini = (int)(1e9);
+    for(int cut = si+1; cut<ei; cut++){
+        int left = mcm(arr, si, cut);
+        int right = mcm(arr, cut, ei);
+
+        mini = min(mini, left + arr[si] * arr[cut] * arr[ei] + right);
+    }
+    return dp[si][ei] = mini;
+}
+
 
 int main(){
     file_i_o();
+    cin>>n;
 }
